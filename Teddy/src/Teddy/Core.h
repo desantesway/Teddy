ab57315d2f156681ab49b52018b50ded467882c0
tekinaii <ilychxry@icloud.com>
2025-06-23 16:10:26 +0100

    enabling asserts on debug

--- Teddy/src/Teddy/Core.h
+++ Teddy/src/Teddy/Core.h
@@ -12,6 +12,11 @@
 	#error "Teddy only supports Windows platform."
 #endif
 
+
+#ifdef TED_DEBUG
+	#define TED_ENABLE_ASSERTS
+#endif
+
 #ifdef TED_ENABLE_ASSERTS
 #define TED_ASSERT(x, ...) { if(!(x)) { TED_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #define TED_CORE_ASSERT(x, ...) { if(!(x)) { TED_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
