ec026edaf89e70928aa9a39612dd0b55b1600e6c
tekinaii <ilychxry@icloud.com>
2025-08-14 18:14:36 +0100

    Added Box2D

--- Teddy/src/Teddy/Core/Log.h
+++ Teddy/src/Teddy/Core/Log.h
@@ -1,9 +1,9 @@
 #pragma once
 
+#include "Teddy/Core/Base.h"
+
 #define GLM_ENABLE_EXPERIMENTAL
 #include "glm/gtx/string_cast.hpp"
-
-#include "Teddy/Core/Base.h"
 
 #pragma warning(push, 0)
 #include "spdlog/spdlog.h"
